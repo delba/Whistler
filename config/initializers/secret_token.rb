d36e4c66497465d7d00fda267ed4a42d0cc8a0e6
Natasha Postolovski <npostolovski@gmail.com>
2013-03-13 15:08:51 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whistler::Application.config.secret_token = 'f4f33166c5e98692f5bf093a49d8db8b2aa72765fae96dcc9a9580a95a18ee412359c67c29619e50fa68dca02715a265ea6a2933cc9c492b0d97d56e7cfdfcde'
