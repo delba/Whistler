93ace1cb6367dff2a89427674d039f48c5970c96
delba <damiendelba@gmail.com>
2013-03-14 20:26:57 -0500

    add unique index to users

--- db/schema.rb
+++ db/schema.rb
@@ -9,19 +9,21 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130314234328) do
+ActiveRecord::Schema.define(version: 20130315012610) do
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
     t.string   "password"
     t.string   "email"
     t.string   "password_confirmation"
   end
+
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
