941249bad180c9e05a562b922fce59646ae1f32b
Natasha Postolovski <npostolovski@gmail.com>
2013-03-14 19:03:43 -0500

    Added a full_name method for user and updated User's profile to show the user's full name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
 	attr_accessible :username, :first_name, :last_name, :email, :password, :password_confirmation
+
+	def full_name
+		"#{first_name} #{last_name}" 
+	end
 end
