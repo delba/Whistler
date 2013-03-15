82089739662f003f0668bf93459637725068caaa
Natasha Postolovski <npostolovski@gmail.com>
2013-03-14 19:50:22 -0500

    The sign-up page works again, and we're using Rails 4.0 now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Whistler::Application.routes.draw do
   get 'sign-up' => 'users#new', as: :new_user
-  get 'profile/:username', 'users#show', as: :user
+  get 'profile/:username' => 'users#show', as: :user
   post 'users' => 'users#create', as: :users
 end
