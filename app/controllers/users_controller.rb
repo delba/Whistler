86523a5af86db81385343997ada37fb1b67955e5
delba <damiendelba@gmail.com>
2013-03-14 20:34:18 -0500

    oups n*2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 	end
 
 	def show
-    @user = User.find_by(user_name: params[:username])
+    @user = User.find_by(username: params[:username])
 	end
 
   private
