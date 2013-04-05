46eae7d6b4249e857014fe96395820535cd54b8e
delba <damiendelba@gmail.com>
2013-04-05 18:14:54 +0200

    custom routes for login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Whistler::Application.routes.draw do
   get  'signup' => 'users#new'
   post 'signup' => 'users#create'
 
+  get    'login'  => 'sessions#new'
+  post   'login'  => 'sessions#create'
+
   get 'profile/:username' => 'users#show', as: :user
   post 'users' => 'users#create', as: :users
 end
