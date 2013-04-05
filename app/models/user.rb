e19eba533b3485506001be1cdb4aa0f0a54f854b
delba <damiendelba@gmail.com>
2013-04-05 17:57:24 +0200

    uniqueness is no more sensitive to the case

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   validates :username, presence: true,
-                       uniqueness: true
+                       uniqueness: { case_sensitive: false }
 
   def full_name
-    "#{first_name} #{last_name}" 
+    "#{first_name} #{last_name}"
   end
 end
