c8b5af9a7f31a145dcdc0722549d306aa5c222a8
delba <damiendelba@gmail.com>
2013-04-06 08:04:30 +0200

    validate uniqueness of email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   validates :username, presence: true,
                        uniqueness: { case_sensitive: false }
   validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
                     email: true
 
   def full_name
