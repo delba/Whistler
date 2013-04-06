8c8f10ebb84d588309a291d517c41e9f603eca1e
delba <damiendelba@gmail.com>
2013-04-06 08:05:22 +0200

    allow the user to login by email or username

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false },
                     email: true
 
+  def self.find_by_login(login)
+    where('lower(email) = :login OR lower(username) = :login',
+          { login: login.downcase }).first
+  end
+
   def full_name
     "#{first_name} #{last_name}"
   end
